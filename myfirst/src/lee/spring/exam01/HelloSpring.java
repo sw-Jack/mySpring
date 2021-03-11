7d165aa89b7cdd38c4d6f3d657fdaa35328f195a
sw-Jack <jacksuwon@gmail.com>
2021-03-12 00:53:43 +0900

    객체 생성 활용

---
+++ myfirst/src/lee/spring/exam01/HelloSpring.java
@@ -0,0 +1,9 @@
+package lee.spring.exam01;
+
+public class HelloSpring {
+
+	public static void main(String[] args) {
+		MyBean bean = new MyBean();
+		bean.sayHello("Spring");
+	}
+}
---
+++ myfirst/src/lee/spring/exam01/MyBean.java
@@ -0,0 +1,7 @@
+package lee.spring.exam01;
+
+public class MyBean {
+	public void sayHello(String name) {
+		System.out.println("Hello, " + name + "!!!");
+	}
+}
