031bc70cee0680d33846ec1d71f58852ee964db5
sw-Jack <jacksuwon@gmail.com>
2021-03-12 00:57:25 +0900

    Spring 설정 파일 applicationContext를 활용한 Bean 객체 사용

---
+++ myfirst/src/lee/spring/exam03/HelloSpring.java
@@ -0,0 +1,15 @@
+package lee.spring.exam03;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class HelloSpring {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext factory = 
+				new GenericXmlApplicationContext("applicationContext.xml");
+		MyBean bean = (MyBean)factory.getBean("myBean");
+		bean.sayHello("Spring");
+		factory.close();
+	}
+}
