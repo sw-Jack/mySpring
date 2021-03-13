46d53f736714f27fec634c89396ee69f4d0a3b24
sw-Jack <jacksuwon@gmail.com>
2021-03-13 20:42:45 +0900

    Annotation 사용 - Component / Autowired / Qualifier / Resource / XML과 Annotation의 병행

--- myboard/src/main/java/lee/spring/polymorphism/LgTv.java
+++ myboard/src/main/java/lee/spring/polymorphism/LgTv.java
@@ -1,7 +1,20 @@
 package lee.spring.polymorphism;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("tv")
 public class LgTv implements TV {
-	@Override
+	@Autowired
+	private Speaker speaker;
+	
+	public LgTv() {
+		System.out.println("Lg TV 객체 생성");
+	}
+
 	public void powerOn() {
 		System.out.println("LgTV 전원을 켠다.");		
 	}
@@ -13,11 +26,11 @@ public class LgTv implements TV {
 
 	@Override
 	public void volumeUp() {
-		System.out.println("LgTV 볼륨을 올린다.");
+		speaker.volumeUp();
 	}
 
 	@Override
 	public void volumeDown() {
-		System.out.println("LgTV 볼륨을 내린다.");
+		speaker.volumeDown();
 	}
 }
