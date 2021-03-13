045067edca9b9c4699e771a2cc45113c1167386a
sw-Jack <jacksuwon@gmail.com>
2021-03-13 20:43:10 +0900

    Annotation 사용 - Component / Autowired / Qualifier / Resource / XML과 Annotation의 병행

--- myboard/src/main/java/lee/spring/polymorphism/SamsungTV.java
+++ myboard/src/main/java/lee/spring/polymorphism/SamsungTV.java
@@ -1,19 +1,66 @@
 package lee.spring.polymorphism;
 
 public class SamsungTV implements TV {
-	public void powerOn() {
-		System.out.println("SamsungTV 전원을 켠다.");
+	private Speaker speaker;
+	private int price;
+	
+	public SamsungTV() {
+		System.out.println("Samsung TV 객체 생성");
 	}
+	
+	public SamsungTV(Speaker speaker) {
+		System.out.println("Samsung TV 객체 생성 : 생성자 injection");
+		this.speaker = speaker;
+	}
+	
+	
+	public SamsungTV(Speaker speaker, int price) {
+		System.out.println("SamsungTV 객체 생성 : 생성자 injection 다중 매핑");
+		this.speaker = speaker;
+		this.price = price;
+	}
+
+	//public void start() {
+	//	System.out.println("객체 초기화 작업 처리");
+	//}
+	
+	//public void stop() {
+	//	System.out.println("객체 삭제 전에 처리할 로직 처리");
+	//}
+	
+
+	public Speaker getSpeaker() {
+		return speaker;
+	}
+	
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("setSpeaker() 호출");
+		this.speaker = speaker;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		System.out.println("setPrice() 호출");
+		this.price = price;
+	}
+	
+	public void powerOn() {
+		System.out.println("SamsungTV 전원을 켠다. (가격 : " + price + "원)");
+	}
+
 
 	public void powerOff() {
 		System.out.println("SamsungTV 전원을 끈다.");
 	}
 
 	public void volumeUp() {
-		System.out.println("SamsungTV 볼륨을 올린다.");
+		speaker.volumeUp();
 	}
 
 	public void volumeDown() {
-		System.out.println("SamsungTV 볼륨을 내린다.");
+		speaker.volumeDown();
 	}
 }
