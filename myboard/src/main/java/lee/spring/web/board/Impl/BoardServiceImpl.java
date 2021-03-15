7d492fe850d4e9654fa32ac932d9ca40cca10309
sw-Jack <jacksuwon@gmail.com>
2021-03-16 01:05:17 +0900

    AOP 구현을 위한 코드 수정

--- myboard/src/main/java/lee/spring/web/board/Impl/BoardServiceImpl.java
+++ myboard/src/main/java/lee/spring/web/board/Impl/BoardServiceImpl.java
@@ -7,35 +7,51 @@ import org.springframework.stereotype.Service;
 
 import lee.spring.web.board.BoardService;
 import lee.spring.web.board.BoardVO;
+import lee.spring.web.common.Log4jAdvice;
+import lee.spring.web.common.LogAdvice;
 
 @Service("boardService")
-public class BoardServiceImple implements BoardService {
+public class BoardServiceImpl implements BoardService {
 
 	@Autowired
 	private BoardDAO boardDAO;
+	//private LogAdvice log;
+	//private Log4jAdvice logging;
+	
+	public BoardServiceImpl() {
+		//logging = new Log4jAdvice();
+	}
 	
 	@Override
 	public void insertBoard(BoardVO vo) {
+		//logging.printLogging();
+		//if(vo.getSeq() == 0) { // 오류 발생 코드
+		//	throw new IllegalArgumentException("0번 글은 등록할 수 없습니다.");
+		//}
 		boardDAO.insertBoard(vo);
 	}
 
 	@Override
 	public void updateBoard(BoardVO vo) {
+		//logging.printLogging();
 		boardDAO.updateBoard(vo);
 	}
 
 	@Override
 	public void deleteBoard(BoardVO vo) {
+		//logging.printLogging();
 		boardDAO.deleteBoard(vo);
 	}
 
 	@Override
 	public BoardVO getBoardVO(BoardVO vo) {
+		//logging.printLogging();
 		return boardDAO.getBoard(vo);
 	}
 
 	@Override
 	public List<BoardVO> getBoardList(BoardVO vo) {
+		//logging.printLogging();
 		return boardDAO.getBoardList(vo);
 	}
 
