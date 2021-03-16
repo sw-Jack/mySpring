1fcb0b8cc273e7ededb093f49d646127852d507e
sw-Jack <jacksuwon@gmail.com>
2021-03-17 01:12:58 +0900

    Spring AOP - 게시글 나타내는 VO 클래스

---
+++ SpringAOP/src/main/java/lee/spring/board/vo/ArticleVO.java
@@ -0,0 +1,14 @@
+package lee.spring.board.vo;
+
+// 게시글 나타내는 ArticleVO 클래스
+public class ArticleVO {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
