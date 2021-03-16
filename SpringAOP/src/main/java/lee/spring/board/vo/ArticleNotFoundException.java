5dd07a50467d27d9d1951b7be614f3cf48e2ddcb
sw-Jack <jacksuwon@gmail.com>
2021-03-17 01:12:31 +0900

    Spring AOP - 사용자 정의 예외 클래스

---
+++ SpringAOP/src/main/java/lee/spring/board/vo/ArticleNotFoundException.java
@@ -0,0 +1,6 @@
+package lee.spring.board.vo;
+
+// 사용자 정의 예외 클래스 : 글이 존재하지 않을 때 발생할 예외
+public class ArticleNotFoundException extends Exception {
+	private static final long serialVersionUID = 1L;
+}
