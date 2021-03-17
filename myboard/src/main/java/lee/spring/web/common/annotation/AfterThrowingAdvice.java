792028022e7d49395cd895b392c81c1b318b6066
sw-Jack <jacksuwon@gmail.com>
2021-03-18 01:04:57 +0900

    Spring AOP - Annotation 활용 (Aspect 클래스)

--- myboard/src/main/java/lee/spring/web/common/annotation/AfterThrowingAdvice.java
+++ myboard/src/main/java/lee/spring/web/common/annotation/AfterThrowingAdvice.java
@@ -12,7 +12,7 @@ public class AfterThrowingAdvice {
 	@Pointcut("execution(* lee.spring.web..*Impl.*(..))")
 	public void allPointCut() {}
 	
-	@AfterThrowing(pointcut = "allPointCut", throwing = "e")
+	@AfterThrowing(pointcut = "allPointCut()", throwing = "e")
 	public void afterThrowingLog(JoinPoint joinPoint, Exception e) {
 		String method = joinPoint.getSignature().getName();
 		System.out.println("[예외처리] " + method + "() 메서드 수행 중 발생된 예외 메세지 : " + e.getMessage());
