a2f57a815811752ec735d3a0aae58eaf217f1a4b
sw-Jack <jacksuwon@gmail.com>
2021-03-18 01:07:07 +0900

    Spring JDBC - DAO

---
+++ myboard/src/main/java/lee/spring/web/board/Impl/boardDAOSpring.java
@@ -0,0 +1,51 @@
+package lee.spring.web.board.Impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import lee.spring.web.board.BoardVO;
+
+@Repository
+public class boardDAOSpring {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	private final String BOARD_INSERT = "insert into myboard(seq, title, writer, content) values((select nvl(max(seq), 0)+1 from myboard), ?, ?, ?)";
+	private final String BOARD_UPDATE = "update myboard set title = ?, content = ? where seq = ?";
+	private final String BOARD_DELETE = "delete myboard where seq = ?";
+	private final String BOARD_GET = "select * from myboard where seq = ?";
+	private final String BOARD_LIST = "select * from myboard order by seq desc";
+	//private final String BOARD_INSERT = "insert into myboard(seq, title, writer, content) values(?, ?, ?, ?)"; // 트랜잭션 오류 처리 테스트 
+
+
+	public void insertBoard(BoardVO vo) {
+		System.out.println("Spring JDBC로 insertBoard() 기능 처리");
+		jdbcTemplate.update(BOARD_INSERT, vo.getTitle(), vo.getWriter(), vo.getContent());
+		//jdbcTemplate.update(BOARD_INSERT, vo.getSeq(), vo.getTitle(), vo.getWriter(), vo.getContent()); // 트랜잭션 오류 처리 테스트 
+
+	}
+
+	public void updateBoard(BoardVO vo) {
+		System.out.println("Spring JDBC로 updateBoard() 기능 처리");
+		jdbcTemplate.update(BOARD_UPDATE, vo.getTitle(), vo.getContent(), vo.getSeq());
+	}
+
+	public void deleteBoard(BoardVO vo) {
+		System.out.println("Spring JDBC로 deleteBoard() 기능 처리");
+		jdbcTemplate.update(BOARD_DELETE, vo.getSeq());
+	}
+
+	public BoardVO getBoard(BoardVO vo) {
+		System.out.println("Spring JDBC로 getBoard() 기능 처리");
+		Object[] args = { vo.getSeq() };
+		return jdbcTemplate.queryForObject(BOARD_GET, new BoardRowMapper(), args);
+	}
+
+	public List<BoardVO> getBoardList(BoardVO vo) {
+		System.out.println("Spring JDBC로 getBoardList() 기능 처리");
+		return jdbcTemplate.query(BOARD_LIST, new BoardRowMapper());
+	}
+}
