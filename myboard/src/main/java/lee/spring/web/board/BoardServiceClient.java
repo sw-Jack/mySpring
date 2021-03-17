c86bcf6720107615bcb0b449b6ccee7a053ba1a4
sw-Jack <jacksuwon@gmail.com>
2021-03-18 01:03:35 +0900

    Spring AOP - around Advice BoardServiceClient main

--- myboard/src/main/java/lee/spring/web/board/BoardServiceClient.java
+++ myboard/src/main/java/lee/spring/web/board/BoardServiceClient.java
@@ -12,7 +12,7 @@ public class BoardServiceClient {
 		// 1. 스프링 컨테이너 구동
 		AbstractApplicationContext container = 
 				new GenericXmlApplicationContext("applicationContextBoard.xml");
-		
+				
 		// 2. 스프링 컨테이너로부터 BoardServiceImple 객체를 lookup 한다
 		BoardService boardService = (BoardService)container.getBean("boardService");
 		
