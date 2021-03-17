698374b4c7e647009d1e11d9d1ae017bc4e2f1d2
sw-Jack <jacksuwon@gmail.com>
2021-03-18 01:02:55 +0900

    Spring JDBC - main

--- myboard/src/main/java/lee/spring/web/board/BoardServiceAnnotationClient.java
+++ myboard/src/main/java/lee/spring/web/board/BoardServiceAnnotationClient.java
@@ -11,13 +11,15 @@ public class BoardServiceAnnotationClient {
 
 		// 1. 스프링 컨테이너 구동
 		AbstractApplicationContext container = 
-				new GenericXmlApplicationContext("applicationContextBoardAnnotation.xml");
+				//new GenericXmlApplicationContext("applicationContextBoardAnnotation.xml");
+				new GenericXmlApplicationContext("applicationContextDB.xml");
 		
 		// 2. 스프링 컨테이너로부터 BoardServiceImple 객체를 lookup 한다
 		BoardService boardService = (BoardService)container.getBean("boardService");
 		
 		// 3. 글 등록 기능 테스트
 		BoardVO vo = new BoardVO();
+		//vo.setSeq(1000); // 트랜잭션 오류 처리 테스트 
 		vo.setTitle("임시제목");
 		vo.setWriter("손");
 		vo.setContent("새로운 글1");
