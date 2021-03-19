0345c14547879f6fefaa50797ae4823f97df1b0e
sw-Jack <jacksuwon@gmail.com>
2021-03-19 20:20:01 +0900

    Spring Transaction - 아이템 정보 기억 클래스

---
+++ SpringTX/src/main/java/lee/spring/store/vo/Item.java
@@ -0,0 +1,27 @@
+package lee.spring.store.vo;
+
+// 아이템 정보를 기억하는 클래스
+public class Item {
+	private Integer id;
+	private String name;
+	private int price;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
