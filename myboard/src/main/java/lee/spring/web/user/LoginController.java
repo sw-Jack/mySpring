f84abbadeefa316fc08587d356636b52c0754e7d
sw-Jack <jacksuwon@gmail.com>
2021-03-23 23:56:40 +0900

    Spring MVC Anntotaion - 로그인 Controller

--- myboard/src/main/java/lee/spring/web/user/LoginController.java
+++ myboard/src/main/java/lee/spring/web/user/LoginController.java
@@ -2,34 +2,37 @@ package lee.spring.web.user;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import lee.spring.web.user.impl.UserDAO;
 
 //로그인 기능 처리 Controller
-public class LoginController implements Controller {
+@Controller
+public class LoginController {
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("로그인 처리");
-		// 1. 사용자 입력 정보 추출
-		String id = request.getParameter("id");
-		String password = request.getParameter("password");
-		// 2. 데이터베이스 연동 처리
-		UserVO vo = new UserVO();
-		vo.setId(id);
-		vo.setPassword(password);
-		UserDAO userDAO = new UserDAO();
+	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
+	public String loginView(UserVO vo) {
+		System.out.println("로그인 화면으로 이동");
+		vo.setId("test");
+		vo.setPassword("test");
+		return "login.jsp";
+	}
+
+	@RequestMapping(value = "/login.do", method = RequestMethod.POST)
+	public String login(UserVO vo, UserDAO userDAO, HttpSession session) {
+		System.out.println("로그인 인증 처리");
 		UserVO user = userDAO.getUser(vo);
-		// 3. 화면 네비게이션
-		ModelAndView mav = new ModelAndView();
-		if(user != null) {
-			mav.setViewName("redirect:getBoardList.do");
+		if (user != null) {
+			session.setAttribute("userName", user.getName());
+			return "getBoardList.do";
 		} else {
-			mav.setViewName("redirect:login.jsp");
+			return "login.jsp";
 		}
-		return mav;
 	}
 }
