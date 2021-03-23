8500d625429c0d9004c3581db410e10f9c9724ad
sw-Jack <jacksuwon@gmail.com>
2021-03-23 23:56:59 +0900

    Spring MVC Anntotaion - 로그아웃 Controller

--- myboard/src/main/java/lee/spring/web/user/LogoutController.java
+++ myboard/src/main/java/lee/spring/web/user/LogoutController.java
@@ -1,24 +1,18 @@
 package lee.spring.web.user;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 //글 삭제 기능 처리 Controller
-public class LogoutController implements Controller {
+@Controller
+public class LogoutController {
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	@RequestMapping("/logout.do")
+	public String logout(HttpSession session) {
 		System.out.println("로그아웃 처리");
-		// 1. 브라우저와 연결된 세션 객체를 종료
-		HttpSession session = request.getSession(false);
 		session.invalidate();
-		// 2. 세션 종료 후 메인 화면으로 이동
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("redirect:login.jsp");
-		return mav;
+		return "login.jsp";
 	}
 }
