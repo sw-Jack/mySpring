7fa8f6390662616ed10f537da8fb32a2efb69685
sw-Jack <jacksuwon@gmail.com>
2021-03-31 21:47:45 +0900

    MainProject Design - Manager DashBoard - Ajax 구현 js

--- DashBoard/src/main/webapp/script.js
+++ DashBoard/src/main/webapp/script.js
@@ -1,3 +1,17 @@
-/**
- * 
- */
+function acyncMovePage(url){
+    
+    var ajaxOption = {
+            url : url,
+            async : true,
+            type : "POST",
+            dataType : "html",
+            cache : false
+    };
+    
+    $.ajax(ajaxOption).done(function(data){
+        $('#bodyContents').children().remove();
+        $('#bodyContents').html(data);
+    });
+}
+
+
